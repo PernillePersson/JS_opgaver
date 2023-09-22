9c2acafbf9f8cc662b2bc68220aa2ea1e9c73140
pernillepersson <pernillekp@me.com>
2023-09-22 09:20:38 +0200

    to-do virker!

--- scripts/todo.js
+++ scripts/todo.js
@@ -9,11 +9,20 @@ function addTD(){
     let i = "l" + liID;
     let text = document.getElementById("newTD").value;
     let li = document.createElement("li");
+
+    var span = document.createElement("SPAN");
+    span.setAttribute("onclick", "removeTD('" + i +"')")
+    var txt = document.createTextNode("x");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
     li.appendChild(document.createTextNode(text));
     li.setAttribute("id", i);
     li.classList.add("td");
     li.setAttribute("onclick", "check('" + i +"')");
     document.getElementById("myTD").appendChild(li);
+
     liID++;
 }
 
