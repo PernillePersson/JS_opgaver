028a91938244b46d7493794f72dedf9040c4aab5
pernillepersson <pernillekp@me.com>
2023-09-22 08:29:50 +0200

    Add virker på to-do.

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,4 +1,23 @@
+var liID = 10;
+
 function check(id){
     document.getElementById(id).classList.toggle("checked");
 
 }
+
+function addTD(){
+    let i = "l" + liID;
+    let text = document.getElementById("newTD").value;
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(text));
+    li.setAttribute("id", i);
+    li.classList.add("td");
+    li.setAttribute("onclick", "check('" + i +"')");
+    document.getElementById("myTD").appendChild(li);
+    liID++;
+}
+
+function removeTD(id){
+    document.getElementById(id).remove();
+}
+
